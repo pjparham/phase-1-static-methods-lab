e47c8519332818ad9d11929f370325fea5fe6e36
Parker Parham <parkerparham@gmail.com>
2022-08-10 11:41:52 -0400

    Done with assignment (reference this later)

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^a-z0-9áéíóúñü' \.,_-]/gim, "");
+  }
+  static titleize(string){
+    let newString = string.replace(/\w\S*/g, function(txt){
+      if (txt === "an" || txt === "the" || txt === "a" || txt === "of" || txt === "and" || txt === "for" || txt === "at" || txt === "but" || txt === "by" || txt === "from"){
+        return txt
+      } 
+      else{
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+  });
+  return this.capitalize(newString)
+  }
   //add static methods here
 }
